7d5160e73c133810467561da58588b969cd5093e
liasica <magicrolan@qq.com>
2026-09-07 11:29:58 +0800

    fix: go install 安装的二进制版本号回落到模块版本
    ldflags 只作用于 goreleaser 构建的二进制，go install 装出来会显示源码里的
    默认值，改为在未注入时从构建信息读取模块版本；同时锁定 goreleaser 版本区间

--- version.go
+++ version.go
@@ -1,4 +1,21 @@
 package swag
 
-// Version of swag.
-var Version = "v2.0.0"
+import "runtime/debug"
+
+const defaultVersion = "v2.0.0"
+
+// Version of swag. 发布的二进制由 ldflags 注入，go install 安装时回落到模块版本
+var Version = defaultVersion
+
+func init() {
+	if Version != defaultVersion {
+		return
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok || info.Main.Version == "" || info.Main.Version == "(devel)" {
+		return
+	}
+
+	Version = info.Main.Version
+}
